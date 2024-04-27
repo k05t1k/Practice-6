06ac0d2087f29ec55a36cad9a56043751fdb0ea2
k05t1k <isip_k.m.fionov@mpt.ru>
2024-04-27 05:23:33 +0300

    Add MVVM

---
+++ Calendar/ViewModel/MainViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Calendar.ViewModel.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calendar.ViewModel
+{
+    internal class MainViewModel : BindableHelper
+    {
+        public MainViewModel() 
+        {
+        
+        }
+    }
+}
